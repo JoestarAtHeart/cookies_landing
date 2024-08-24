6995cedcbd193b4dea764bb816b1cbf7c0a04479
Tima <gtv2002@inbox.ru>
2024-08-24 13:26:10 +0700

    form validation

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// Скролл по ссылкам
 document.getElementById("main-action-button").onclick = function() {
     document.getElementById("products").scrollIntoView({behavior: "smooth"});
 }
@@ -16,6 +17,7 @@ for (let i = 0; i < buttons.length; i++) {
     }
 }
 
+// Переключение валют
 const prices = document.getElementsByClassName("products-item-price");
 document.getElementById("change-currency").onclick = function(e) {
     const currentCurrency = e.target.innerText;
@@ -31,5 +33,28 @@ document.getElementById("change-currency").onclick = function(e) {
     e.target.innerText = newCurrency;
     for (let i = 0; i < prices.length; i++) {
         prices[i].innerText = +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) + " " + newCurrency;
+    }
+}
+
+// Валидация формы
+const product = document.getElementById("product");
+const name = document.getElementById("name");
+const phone = document.getElementById("phone");
+document.getElementById("order-action").onclick = function() {
+    let hasError = false;
+    [product, name, phone].forEach(item => {
+        if (!item.value) {
+            item.style.borderColor = "red";
+            hasError = true;
+        } else {
+            item.style.borderColor = ""; 
+        }
+    });
+
+    if (!hasError) {
+        [product, name, phone].forEach(item => {
+           item.value = "";
+        });
+        alert("Спасибо за заказ! Мы скоро свяжемся с вами!");
     }
 }
